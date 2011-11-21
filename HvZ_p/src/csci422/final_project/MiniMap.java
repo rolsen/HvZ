4acea1d5947d6780fe57436d78dfe719a4dc215d
Kevin Raber <kraber@mymail.mines.edu>
2011-11-20 23:33:02 -0700

    mini map worked for me...uncommented Rory's work

--- HvZ_p/src/csci422/final_project/MiniMap.java
+++ HvZ_p/src/csci422/final_project/MiniMap.java
@@ -1,50 +1,56 @@
 package csci422.final_project;
 
+import java.util.ArrayList;
+import java.util.List;
+import android.content.Context;
+import android.graphics.*;
+import android.location.*;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.*;
 
-import com.google.android.maps.MapActivity;
+import com.google.android.maps.*;
 
 public class MiniMap extends MapActivity {
-//	MapView mapView;
-//	MapController mapController;
-//
-//	int DEFAULT_ZOOM_LEVEL = 19;
-//	int KAFADAR_LAT = 39751265;
-//	int KAFADAR_LNG = -105221450;
-//	int CH_LAT = 39751702;
-//	int CH_LNG = -105222700;
-//	int A_POINT_LAT = 39751202;
-//	int A_POINT_LNG = -105220700;
-//
-//	class FlareOverlay extends com.google.android.maps.Overlay
-//	{
-//		GeoPoint point;
-//		
-//		public FlareOverlay(GeoPoint gp) {
-//			point = gp;
-//		}
-//		
-//		@Override
-//		public boolean draw(Canvas canvas, MapView mapView, 
-//				boolean shadow, long when)
-//		{
-//			super.draw(canvas, mapView, shadow);
-//
-//			//---translate the GeoPoint to screen pixels---
-//			Point screenPts = new Point();
-//			if (point == null) {
-//				point = new GeoPoint(KAFADAR_LAT, KAFADAR_LNG);
-//			}
-//			mapView.getProjection().toPixels(point, screenPts);
-//
-//			//---add the marker---
-//			Bitmap bmp = BitmapFactory.decodeResource(
-//					getResources(), R.drawable.flare_t);
-//			canvas.drawBitmap(bmp, screenPts.x, screenPts.y, null);
-//			return true;
-//		}
-//	}
+	MapView mapView;
+	MapController mapController;
+
+	int DEFAULT_ZOOM_LEVEL = 19;
+	int KAFADAR_LAT = 39751265;
+	int KAFADAR_LNG = -105221450;
+	int CH_LAT = 39751702;
+	int CH_LNG = -105222700;
+	int A_POINT_LAT = 39751202;
+	int A_POINT_LNG = -105220700;
+
+	class FlareOverlay extends com.google.android.maps.Overlay
+	{
+		GeoPoint point;
+		
+		public FlareOverlay(GeoPoint gp) {
+			point = gp;
+		}
+		
+		public boolean draw(Canvas canvas, MapView mapView, 
+				boolean shadow, long when)
+		{
+			super.draw(canvas, mapView, shadow);
+
+			//---translate the GeoPoint to screen pixels---
+			Point screenPts = new Point();
+			if (point == null) {
+				point = new GeoPoint(KAFADAR_LAT, KAFADAR_LNG);
+			}
+			mapView.getProjection().toPixels(point, screenPts);
+
+			//---add the marker---
+			Bitmap bmp = BitmapFactory.decodeResource(
+					getResources(), R.drawable.flare_t);
+			canvas.drawBitmap(bmp, screenPts.x, screenPts.y, null);
+			return true;
+		}
+	}
 
 	/** Called when the activity is first created. */
 	@Override
@@ -54,26 +60,26 @@ public class MiniMap extends MapActivity {
 
 		Log.w("fixing", "HAI this works");
 
-//		mapView = (MapView) findViewById(R.id.mapView);  
-//		mapView.setBuiltInZoomControls(true);
-//		mapView.displayZoomControls(true);
-//		mapView.setSatellite(true);
-//
-//		mapController = mapView.getController();
-//
-//		GeoPoint kafadar = new GeoPoint(KAFADAR_LAT,KAFADAR_LNG);
-//
-//		mapController.animateTo(kafadar);
-//		mapController.setZoom(DEFAULT_ZOOM_LEVEL);
-//		
-//		displayFlares();
-//
-//        final Button button = (Button) findViewById(R.id.flare);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                shootFlare();
-//            }
-//        });
+		mapView = (MapView) findViewById(R.id.mapView);  
+		mapView.setBuiltInZoomControls(true);
+		mapView.displayZoomControls(true);
+		mapView.setSatellite(true);
+
+		mapController = mapView.getController();
+
+		GeoPoint kafadar = new GeoPoint(KAFADAR_LAT,KAFADAR_LNG);
+
+		mapController.animateTo(kafadar);
+		mapController.setZoom(DEFAULT_ZOOM_LEVEL);
+		
+		displayFlares();
+
+        final Button button = (Button) findViewById(R.id.flare);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                shootFlare();
+            }
+        });
 
 	}
 
@@ -82,50 +88,50 @@ public class MiniMap extends MapActivity {
 		return false;
 	}
 
-//	public Location getUserLocation() {
-//		// Acquire a reference to the system Location Manager
-//		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-//
-//		// TODO: This just sets the location for testing purposes to be in Kafadar (CSM)
-//		// GPS_PROVIDER is necessary for setTestProviderLocation and getLastKnowLocation to work together
-//		// Begin TEMP
-//		Location kafadarLocation = new Location(LocationManager.GPS_PROVIDER);
-//		kafadarLocation.setLatitude(KAFADAR_LAT);
-//		kafadarLocation.setLongitude(KAFADAR_LNG);
-//		locationManager.setTestProviderLocation (LocationManager.GPS_PROVIDER, kafadarLocation);
-//		// End TEMP
-//
-//		return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-//	}
-//	
-//	public void displayFlares() {
-//		List<GeoPoint> list = getFlareLocations();
-//		List<Overlay> listOfOverlays = mapView.getOverlays();
-//		listOfOverlays.clear();
-//		
-//		for (GeoPoint point : list) {			
-//			FlareOverlay mapOverlay = new FlareOverlay(point);
-//			listOfOverlays.add(mapOverlay);
-//		}
-//		
-//		mapView.invalidate(); // Calls onDraw()
-//	}
-//	
-//	public List<GeoPoint> getFlareLocations() {
-//		List<GeoPoint> list = new ArrayList<GeoPoint>();
-//		
-//		// TODO: Get actual locations from server
-//		// Begin TEMP
-//		GeoPoint near_ch = new GeoPoint(CH_LAT,CH_LNG);
-//		list.add(near_ch);
-//		
-//
-//		GeoPoint a_point = new GeoPoint(A_POINT_LAT,A_POINT_LNG);
-//		list.add(a_point);
-//		// End TEMP
-//		
-//		return list;
-//	}
+	public Location getUserLocation() {
+		// Acquire a reference to the system Location Manager
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+		// TODO: This just sets the location for testing purposes to be in Kafadar (CSM)
+		// GPS_PROVIDER is necessary for setTestProviderLocation and getLastKnowLocation to work together
+		// Begin TEMP
+		Location kafadarLocation = new Location(LocationManager.GPS_PROVIDER);
+		kafadarLocation.setLatitude(KAFADAR_LAT);
+		kafadarLocation.setLongitude(KAFADAR_LNG);
+		locationManager.setTestProviderLocation (LocationManager.GPS_PROVIDER, kafadarLocation);
+		// End TEMP
+
+		return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	}
+	
+	public void displayFlares() {
+		List<GeoPoint> list = getFlareLocations();
+		List<Overlay> listOfOverlays = mapView.getOverlays();
+		listOfOverlays.clear();
+		
+		for (GeoPoint point : list) {			
+			FlareOverlay mapOverlay = new FlareOverlay(point);
+			listOfOverlays.add(mapOverlay);
+		}
+		
+		mapView.invalidate(); // Calls onDraw()
+	}
+	
+	public List<GeoPoint> getFlareLocations() {
+		List<GeoPoint> list = new ArrayList<GeoPoint>();
+		
+		// TODO: Get actual locations from server
+		// Begin TEMP
+		GeoPoint near_ch = new GeoPoint(CH_LAT,CH_LNG);
+		list.add(near_ch);
+		
+
+		GeoPoint a_point = new GeoPoint(A_POINT_LAT,A_POINT_LNG);
+		list.add(a_point);
+		// End TEMP
+		
+		return list;
+	}
 	
 	public void shootFlare() {
 		// TODO: send Flare data to server, update screen
