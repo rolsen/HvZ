ca22c7f0540df579815f56ffbf7e105c8bea6627
Rory Olsen <rolsen@mines.edu>
2011-11-20 20:04:50 -0700

    Adding mini map functionality.

--- HvZ_p/gen/csci422/final_project/R.java
+++ HvZ_p/gen/csci422/final_project/R.java
@@ -11,29 +11,35 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_hvz=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int flare_t=0x7f020000;
+        public static final int ic_hvz=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f050007;
+        public static final int LinearLayout1=0x7f05000b;
         public static final int button1=0x7f050001;
         public static final int button2=0x7f050002;
-        public static final int button3=0x7f050003;
-        public static final int button4=0x7f050004;
-        public static final int button5=0x7f050005;
-        public static final int date=0x7f05000b;
-        public static final int human=0x7f050009;
+        public static final int date=0x7f05000f;
+        public static final int flare=0x7f05000a;
+        public static final int human=0x7f05000d;
         public static final int linearLayout1=0x7f050000;
+        public static final int mapView=0x7f050008;
         public static final int menu_content=0x7f050006;
-        public static final int reportKill=0x7f05000c;
-        public static final int time=0x7f05000a;
-        public static final int zombie=0x7f050008;
+        public static final int mini_map=0x7f050003;
+        public static final int outerMap=0x7f050007;
+        public static final int reportKill=0x7f050010;
+        public static final int shoot_flare=0x7f050004;
+        public static final int strategy_map=0x7f050005;
+        public static final int time=0x7f05000e;
+        public static final int zombie=0x7f05000c;
+        public static final int zoom=0x7f050009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int menu=0x7f030001;
-        public static final int players=0x7f030002;
-        public static final int report=0x7f030003;
+        public static final int mini_map=0x7f030002;
+        public static final int players=0x7f030003;
+        public static final int report=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
