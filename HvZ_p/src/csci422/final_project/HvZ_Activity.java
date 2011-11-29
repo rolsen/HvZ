ed6e920737ef2d7e2218940ccc965233a44e1206
Rory Olsen <rolsen@mines.edu>
2011-11-29 10:45:36 -0700

    Just ignore this commit

--- HvZ_p/src/csci422/final_project/HvZ_Activity.java
+++ HvZ_p/src/csci422/final_project/HvZ_Activity.java
@@ -5,7 +5,6 @@ import java.io.File;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
