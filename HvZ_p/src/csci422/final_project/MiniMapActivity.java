1f5622c39f736fd9ac6831584c050a01d5dbb986
Rory Olsen <rolsen@mines.edu>
2011-12-05 02:51:44 -0700

    Progress on location listener, needs a real device to test it now

--- HvZ_p/src/csci422/final_project/MiniMapActivity.java
+++ HvZ_p/src/csci422/final_project/MiniMapActivity.java
@@ -14,8 +14,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -30,6 +32,13 @@ import com.google.android.maps.Overlay;
 public class MiniMapActivity extends MapActivity {
 	MapView mapView;
 	MapController mapController;
+	LocationManager locationManager;
+	GeoPoint userLocation;
+
+	private static final String NETWORK = LocationManager.NETWORK_PROVIDER;
+	private static final String GPS = LocationManager.GPS_PROVIDER;
+	private static final int UPDATE_MIN_TIME = 60000;
+	private static final int UPDATE_MIN_DIST = 10;
 
 	int DEFAULT_ZOOM_LEVEL = 18;
 	int KAFADAR_MICRO_LAT = 39751265;
@@ -40,21 +49,21 @@ public class MiniMapActivity extends MapActivity {
 	int A_POINT_MICRO_LNG = -105220700;
 	int BROWN_MICRO_LAT = 39749784;
 	int BROWN_MICRO_LNG = -105221247;
-	
+
 	private static final String DEFAULT_SERVER_URL = "http://inside.mines.edu/~rolsen/";
 	private static final String REPORT_FLARE_ACTION = "cgi-bin/flareReport.cgi";
 	private static final String FLARE_LIST = "cgi-bin/flareList.cgi";
-	
+
 	private static final String GENERAL_ERROR = "Something went wrong.";
-	
+
 	class FlareOverlay extends com.google.android.maps.Overlay
 	{
 		GeoPoint point;
-		
+
 		public FlareOverlay(GeoPoint gp) {
 			point = gp;
 		}
-		
+
 		public boolean draw(Canvas canvas, MapView mapView, 
 				boolean shadow, long when)
 		{
@@ -62,7 +71,7 @@ public class MiniMapActivity extends MapActivity {
 				System.out.println("Error in draw flare, GeoPoint is null");
 				return false;
 			}
-			
+
 			super.draw(canvas, mapView, shadow);
 
 			// Translate the GeoPoint to screen pixels
@@ -91,79 +100,75 @@ public class MiniMapActivity extends MapActivity {
 		mapView.setSatellite(true);
 
 		mapController = mapView.getController();
-
-		
-		//GeoPoint brown = new GeoPoint(BROWN_LAT,BROWN_LNG); // TODO: make this the user location
-		GeoPoint brown = geoPointFromLocation(getUserLocation());
-		
-		mapController.animateTo(brown);
 		mapController.setZoom(DEFAULT_ZOOM_LEVEL);
-		
-		displayFlares();
 
-        final Button button = (Button) findViewById(R.id.flare);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                shootFlare();
-            }
-        });
+		drawMap();
 
-        Bundle b = getIntent().getExtras();
-        if ((b != null) && b.getBoolean("shootFlare")) {
-        	shootFlare();
-        }
-        
-        System.out.println("MiniMapActivity has finished onCreate");
+		final Button button = (Button) findViewById(R.id.flare);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				shootFlare();
+			}
+		});
+
+		Bundle b = getIntent().getExtras();
+		if ((b != null) && b.getBoolean("shootFlare")) {
+			shootFlare();
+		}
+
+		if (isRealPhone()) {
+			initiateLocationListening();
+		}
+
+		System.out.println("MiniMapActivity has finished onCreate");
 	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
-	
+
 	public GeoPoint geoPointFromLocation(Location l) {
 		int lat = (int) (l.getLatitude() * 1e6);
 		int lng = (int) (l.getLongitude() * 1e6);
 		GeoPoint gp = new GeoPoint(lat, lng);
-		
+
 		// System.out.println(String.format("The lat: was %f now %d, the lng: was %f now %d", l.getLatitude(), lat, l.getLongitude(), lng));
 		return gp;
 	}
 
-	public Location getUserLocation() {
+	public GeoPoint getUserLocation() {
 		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-		try {
-			// Acquire a reference to the system Location Manager
-			System.out.println("getUserLocation stub, setting user location to Brown Building");
-	
-			// TODO: This just sets the location for testing purposes to be in Kafadar (CSM)
-			// GPS_PROVIDER is necessary for setTestProviderLocation and getLastKnowLocation to work together
-			// Begin TEMP
-			Location userLocation = new Location(LocationManager.GPS_PROVIDER);
-			userLocation.setLatitude(BROWN_MICRO_LAT / 1e6);
-			userLocation.setLongitude(BROWN_MICRO_LNG / 1e6);
-			locationManager.setTestProviderLocation (LocationManager.GPS_PROVIDER, userLocation);
-			// End TEMP
-	
-			return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		if (isRealPhone()) {
+			if (userLocation != null) {
+				return userLocation;
+			}
+
+			// NETWORK instead of GPS because I'm guessing GPS will take too long,
+			// 		especially if the user is inside.
+			return geoPointFromLocation(locationManager.getLastKnownLocation(NETWORK));
 		}
-		catch (IllegalArgumentException e) {
+		else {
 			System.out.println("getUserLocation detects emulator instead of real device");
-			
-			Location userLocation = new Location(LocationManager.GPS_PROVIDER);
-			userLocation.setLatitude(BROWN_MICRO_LAT / 1e6);
-			userLocation.setLongitude(BROWN_MICRO_LNG / 1e6);
-			
-			return userLocation;
+
+			Location testLocation = new Location(GPS);
+			testLocation.setLatitude(BROWN_MICRO_LAT / 1e6);
+			testLocation.setLongitude(BROWN_MICRO_LNG / 1e6);
+
+			return geoPointFromLocation(testLocation);
 		}
 	}
-	
-	public void displayFlares() {
+
+	public void drawMap() {
+		userLocation = getUserLocation();
+
+		mapController.animateTo(userLocation);
+
 		List<GeoPoint> list = getFlareLocations();
 		List<Overlay> listOfOverlays = mapView.getOverlays();
 		listOfOverlays.clear();
-		
+
 		if (list != null) {
 			for (GeoPoint point : list) {
 				FlareOverlay mapOverlay = new FlareOverlay(point);
@@ -173,29 +178,29 @@ public class MiniMapActivity extends MapActivity {
 		}
 		mapView.invalidate(); // Calls onDraw()
 	}
-	
+
 	// May return a null List
 	public List<GeoPoint> getFlareLocations() {
 		List<GeoPoint> list = new ArrayList<GeoPoint>();
-		
+
 		// Begin TEMP
-//		GeoPoint near_ch = new GeoPoint(CH_MICRO_LAT,CH_MICRO_LNG);
-//		list.add(near_ch);
-//		
-//		GeoPoint a_point = new GeoPoint(A_POINT_MICRO_LAT,A_POINT_MICRO_LNG);
-//		list.add(a_point);
+		//		GeoPoint near_ch = new GeoPoint(CH_MICRO_LAT,CH_MICRO_LNG);
+		//		list.add(near_ch);
+		//		
+		//		GeoPoint a_point = new GeoPoint(A_POINT_MICRO_LAT,A_POINT_MICRO_LNG);
+		//		list.add(a_point);
 		// End TEMP
-		
+
 		try {
 			URL flareListURL = new URL(getFlareListURL());
-			
+
 			BufferedReader in = new BufferedReader(
 					new InputStreamReader(flareListURL.openStream()));
 
 			String inputLine;
 			String pipeDelimiter = "\\|";
 			int lat, lng;
-			
+
 			inputLine = in.readLine();
 			while (inputLine != null) {
 				// inputLine = inputLine.replace("\n", "");
@@ -203,61 +208,99 @@ public class MiniMapActivity extends MapActivity {
 					inputLine = in.readLine();
 					continue;
 				}
-				
+
 				System.out.printf("LINEE: %s\n", inputLine);
-				
+
 				String[] tokens  = inputLine.split(pipeDelimiter);
-				
+
 				if (tokens.length < 3) {
 					return list;
 				}
-				
+
 				System.out.printf("line: %s, %s\n", tokens[0], tokens[1]);
 				lat = Integer.parseInt(tokens[0]);
 				lng = Integer.parseInt(tokens[1]);
 				list.add(new GeoPoint(lat, lng));
-				
+
 				inputLine = in.readLine();
 			}
-			
+
 			in.close();
 		} catch (MalformedURLException e) {
 			Toast.makeText(getApplicationContext(), GENERAL_ERROR, Toast.LENGTH_LONG).show();
 		} catch (IOException e) {
 			Toast.makeText(getApplicationContext(), GENERAL_ERROR, Toast.LENGTH_LONG).show();
 		}
-		
+
 		return list;
 	}
-	
+
 	public void shootFlare() {
 		// TODO: send Flare data to server, update screen
 		System.out.println("Placeholder flare");
-		
+
 		List<GeoPoint> list = getFlareLocations();
 		List<Overlay> listOfOverlays = mapView.getOverlays();
 		listOfOverlays.clear();
-		
+
 		for (GeoPoint point : list) {
 			FlareOverlay mapOverlay = new FlareOverlay(point);
 			listOfOverlays.add(mapOverlay);
 		}
-		
+
 		GeoPoint brown_point = new GeoPoint(BROWN_MICRO_LAT, BROWN_MICRO_LNG);
 		FlareOverlay mapOverlay = new FlareOverlay(brown_point);
 		listOfOverlays.add(mapOverlay);
-		
+
 		mapView.invalidate(); // Calls onDraw()
-		
+
 		Toast t = Toast.makeText(getApplicationContext(), "You have fired your flare gun!", Toast.LENGTH_LONG);
 		t.show();
 	}
-	
+
+	public void updateUserLocation(Location loc) {
+		userLocation = geoPointFromLocation(loc);
+		drawMap();
+	}
+
+	public void initiateLocationListening() {
+		// Define a listener that responds to location updates
+		LocationListener locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				updateUserLocation(location);
+			}
+
+			public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+			public void onProviderEnabled(String provider) {}
+
+			public void onProviderDisabled(String provider) {}
+		};
+
+		// TODO: The following two lines might need some optimizing
+		// Register the listener with the Location Manager to receive location updates
+		locationManager.requestLocationUpdates(GPS, UPDATE_MIN_TIME, UPDATE_MIN_DIST, locationListener);
+		locationManager.requestLocationUpdates(NETWORK, UPDATE_MIN_TIME, UPDATE_MIN_DIST, locationListener);
+
+	}
+
 	public String getFlareReportURL() {
 		return DEFAULT_SERVER_URL + REPORT_FLARE_ACTION;
 	}
-	
+
 	public String getFlareListURL() {
 		return DEFAULT_SERVER_URL + FLARE_LIST;
+	}
+
+	public boolean isRealPhone() {
+		// This is a shoddy hack of a way to tell whether or not this is running on an
+		//		emulator or not, but Android has no official way to do it. For production
+		// 		code, remove/comment out all the TelephonyManager stuff, the if statement,
+		// 		and the READ_PHONE_STATE permission in the Manifest
+		TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+		if (Integer.parseInt(tm.getDeviceId()) != 0) {
+			return true;
+		}
+		return false;
 	}
 }
